0a59d5efb1a16481e64e7359825a966c8e850de9
adriano abner <adriano.abner@live.com>
2022-03-12 21:53:12 -0300

    Adicionando mecânica de itens

--- src/js/Itens/itens.js
+++ src/js/Itens/itens.js
@@ -1,19 +1,30 @@
-let equipamentos =[
+
+
+let itens =[
     {
         nome       : "",
         vida       : 0,
-        quantidade : 0,
+        ataque     : 0,
         defesa     : 0,
+        velocidade : 0,
+        tempo      : 0,   
+        quantidade : 0,
         id         : "",
-        tipo       : ""   
+        fome       : 0,  
+        tipo       : "", 
     },
     {
         nome       : "Carne",
-        vida       : +5,
-        quantidade : 1,
+        vida       : +5555,
+        ataque     : 0,
         defesa     : 0,
+        velocidade : 0,
+        tempo      : 1,
+        quantidade : 1,
         id         : "9000",
+        fome       : +20,   
         tipo       : "comida",
-        fome       : +20   
     },
 ]
+
+export {itens}
